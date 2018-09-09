4a46198b6b32f7fe58e94ab57c5216b11db740a9
mjbenefiel <michaeljbenefiel@gmail.com>
2018-09-09 07:40:51 -0500

    09092018 commits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,120 +1,162 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var questions = null;
-var answers = null;
+var contestant;
 
-function startGame () {
+var rightAnswer = 0;
 
-var questionOne = {
-    id: 1,
-    question: "What is the answer to question 1?",
-    options: ["A", "B", "C", "D"],
-    answer: "A",
-    img: "assets/images/#.png"
+var wrongAnswer = 0;
 
-}
+var unAnswer = 0;
 
-var questionTwo = {
-    id: 2,
-    question: "What is the answer to question 2?",
-    options: ["A", "B", "C", "D"],
-    answer: "B",
-    img: "assets/images/#.png"
+var question = 0;
 
-}
-
-var questionThree = {
-    id: 3,
-    question: "What is the answer to question 3?",
-    options: ["A", "B", "C", "D"],
-    answer: "C",
-    img: "assets/images/#.png"
-
-}
-
-var questionFour = {
-    id: 4,
-    question: "What is the answer to question 4?",
-    options: ["A", "B", "C", "D"],
-    answer: "A",
-    img: "assets/images/#.png"
-
-}
-
-var questionFive = {
-    id: 5,
-    question: "What is the answer to question 5?",
-    options: ["A", "B", "C", "D"],
-    answer: "D",
-    img: "assets/images/#.png"
-
-}
-
-var questionSix = {
-    id: 6,
-    question: "What is the answer to question 6?",
-    options: ["A", "B", "C", "D"],
-    answer: "D",
-    img: "assets/images/#.png"
-
-}
-
-var questionSeven = {
-    id: 7,
-    question: "What is the answer to question 7?",
-    options: ["A", "B", "C", "D"],
-    answer: "C",
-    img: "assets/images/#.png"
-
-}
-
-var questionEight = {
-    id: 8,
-    question: "What is the answer to question 8?",
-    options: ["A", "B", "C", "D"],
-    answer: "A",
-    img: "assets/images/#.png"
-
-}
-
-questions = null // reset question
-answers = null // reset answer
-
-//array to store all questions
-var questionSelection = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight]
-//function to display all questions and options
-function questionDisplay() {
-$.each(questionSelection, function (index, questions) {
-    // div created so wrestler displays in correct area with "success" panel
-    var newQuestionDiv = $("<div>").addClass("character panel panel-success").attr("id", questions.id);
-
-    $("<div>").addClass("panel-heading").html(questions.question).appendTo(newQuestionDiv);
-    $("<div>").addClass("panel-body").append(questions.options).appendTo(newQuestionDiv);
-    // $("<div>").addClass("panel-footer").append("<span class='hp'>" + wrestler.healthPoints + "</span>").appendTo(newCharacterDiv);
-
-    // append new div to character selection
-    $("#question-area").append(newQuestionDiv);
-});
-};
-   
-    //beginning of game, on click
-    $("#start").on("click", function(){
-        $("#start").remove();
-        // $("#question-area").append(newQuestionDiv);
-        questionDisplay();
-                
-        });
+var images;
+    
+    var questions = null;
+	var answers = null;
+	var counter = 3;
 
 
-}; //END OF startGame();
+	function startGame() {
 
-startGame();
+        var historyQuestions = [
+        {
+        
+    
+			question: "What is the answer to question 1?",
+			options: ["A", "B", "C", "D"],
+			answer: 0,
+			img: "assets/images/#.png"
 
+		},
 
+		{
+			
+			question: "What is the answer to question 2?",
+			options: ["A", "B", "C", "D"],
+			answer: 1,
+			img: "assets/images/#.png"
 
+		},
 
+		{
+			
+			question: "What is the answer to question 3?",
+			options: ["A", "B", "C", "D"],
+			answer: 2,
+			img: "assets/images/#.png"
 
+		},
 
+		{
+			
+			question: "What is the answer to question 4?",
+			options: ["A", "B", "C", "D"],
+			answer: 0,
+			img: "assets/images/#.png"
+
+		},
+
+		{
+			
+			question: "What is the answer to question 5?",
+			options: ["A", "B", "C", "D"],
+			answer: 3,
+			img: "assets/images/#.png"
+
+		},
+
+		{
+			
+			question: "What is the answer to question 6?",
+			options: ["A", "B", "C", "D"],
+			answer: 2,
+			img: "assets/images/#.png"
+
+		},
+
+		{
+			
+			question: "What is the answer to question 7?",
+			options: ["A", "B", "C", "D"],
+			answer: 1,
+			img: "assets/images/#.png"
+
+		},
+
+		{
+			
+			question: "What is the answer to question 8?",
+			options: ["A", "B", "C", "D"],
+			answer: 0,
+			img: "assets/images/#.png"
+
+        },
+        
+    ]
+        // var questionArr = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight]
+
+        var timerId;
+		//beginning of game, on click
+		$("#start").on("click", function () {
+			$(this).hide();
+			timerId = setInterval(timer, 1000)
+            displayQuestion();
+            timer();
+        
+
+		});
+
+		function timer() {
+            $(".timer").html("Time remaining: " + " " + counter + " secs");
+               counter--;
+               if (counter < 0) {
+				clearInterval(timerId);
+				return;
+			}
+
+			
+		}
+        
+        function displayQuestion() {
+            $("#question-area").html(historyQuestions[0].question);
+            question++;
+            
+            var optionArr = historyQuestions[0].options;
+              var buttonsArr = [];
+            
+              for (let i = 0; i < optionArr.length; i++) {
+                var button = $('<button>');
+                button.text(optionArr[i]);
+                button.attr('button-id', i);
+                $('#options').append(button);
+               }
+               
+               selectedOption();
+            
+              } 
+         function selectedOption() {
+             $('#options').on('click', 'button', function(e){
+            // contestant = $(this).data("id");
+             historyQuestions[0].answer;
+             if(contestant != historyQuestions[0].answer) {
+            
+             $('#result').html("Wrong Answer! The correct answer is AAAAA");
+             wrongAnswer++;
+            
+            } else if (contestant === historyQuestions[0].answer) {
+            $('#result').html("Correct!!! The pet tiger name is aaaaaaaaaaaaaaaa");
+            rightAnswer++;
+            
+            }
+            
+            });
+
+        }   
+
+	}; //END OF startGame();
+
+	startGame();
 
 
 }); //END OF (document).ready
