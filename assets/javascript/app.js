baf8253b44aaf1fafa45b540c8260dbaf4e5a219
mjbenefiel <michaeljbenefiel@gmail.com>
2018-09-10 18:08:50 -0500

    09102018 commits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,158 +1,107 @@
 $(document).ready(function () {
 
     var contestant;
-    
+
+    var currentQuestion = 0;
+
     var rightAnswer = 0;
-    
+
     var wrongAnswer = 0;
-    
+
     var unAnswer = 0;
-    
+
     var question = 0;
-    
+
     var images;
-        
-        var questions = null;
-        var answers = null;
-        var counter = 3;
-    
-    
-        function startGame() {
-    
-            var historyQuestions = [
+
+    var questions = null;
+    var answers = null;
+    var counter = 3;
+
+
+    function startGame() {
+
+        var historyQuestions = [
             {
-            
-        
+
+
                 question: "What is the answer to question 1?",
                 options: ["A", "B", "C", "D"],
-                answer: "A",
+                answer: [true, false, false, false],
                 img: "assets/images/#.png"
-    
+
             },
-    
+
             {
-                
+
                 question: "What is the answer to question 2?",
                 options: ["A", "B", "C", "D"],
-                answer: "0",
+                answer: [false, true, false, false],
                 img: "assets/images/#.png"
-    
+
             },
-    
+
             {
-                
+
                 question: "What is the answer to question 3?",
                 options: ["A", "B", "C", "D"],
-                answer: 2,
+                answer: [true, false, false, false],
                 img: "assets/images/#.png"
-    
-            },
-    
-            {
-                
-                question: "What is the answer to question 4?",
-                options: ["A", "B", "C", "D"],
-                answer: 0,
-                img: "assets/images/#.png"
-    
-            },
-    
-            {
-                
-                question: "What is the answer to question 5?",
-                options: ["A", "B", "C", "D"],
-                answer: 3,
-                img: "assets/images/#.png"
-    
-            },
-    
-            {
-                
-                question: "What is the answer to question 6?",
-                options: ["A", "B", "C", "D"],
-                answer: 2,
-                img: "assets/images/#.png"
-    
-            },
-    
-            {
-                
-                question: "What is the answer to question 7?",
-                options: ["A", "B", "C", "D"],
-                answer: 1,
-                img: "assets/images/#.png"
-    
-            },
-    
-            {
-                
-                question: "What is the answer to question 8?",
-                options: ["A", "B", "C", "D"],
-                answer: 0,
-                img: "assets/images/#.png"
-    
-            },
-            
-        ]
-            // var questionArr = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight]
-    
-            var timerId;
-            //beginning of game, on click
-            $("#start").on("click", function () {
-                $(this).hide();
-                timerId = setInterval(timer, 1000)
-                displayQuestion();
-                timer();
-            
-    
-            });
-    
-            function timer() {
-                $(".timer").html("Time remaining: " + " " + counter + " secs");
-                   counter--;
-                   if (counter < 0) {
-                    clearInterval(timerId);
-                    return;
-                }
-    
-                
-            }
-            
-            function displayQuestion() {
-                $("#question-area").html(historyQuestions[0].question);
-                question++;
-                
-                var optionArr = historyQuestions[0].options;
-                  var buttonsArr = [];
-                
-                  for (let i = 0; i < optionArr.length; i++) {
-                    var button = $('<button>');
-                    button.text(optionArr[i]);
-                    button.attr('id', i);
-                    $('#options').append(button);
-                   }
-                   
-                   selectedOption0();
-                  
-                
-                  } 
-             function selectedOption0() {
-               $('#0').on('click', function(e){
-                 $('#result').html("Correct!!! The pet tiger name is aaaaaaaaaaaaaaaa");
-                    rightAnswer++;
-                                 
-                           
-                
-                });
 
-                  
-          
-    
-            }   
-    
-        }; //END OF startGame();
-    
-        startGame();
-    
-    
-    }); //END OF (document).ready
+            },
+
+
+        ]
+        // var questionArr = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight]
+
+        var timerId;
+        //beginning of game, on click
+        $("#start").on("click", function () {
+            $(this).hide();
+            timerId = setInterval(timer, 1000)
+            displayQuestionAnswers();
+            timer();
+
+
+        });
+
+        function timer() {
+            $(".timer").html("Time remaining: " + " " + counter + " secs");
+            counter--;
+            if (counter < 0) {
+                clearInterval(timerId);
+                return;
+            }
+
+
+        }
+
+        function displayQuestionAnswers() {
+            $("#question-area").html(historyQuestions[currentQuestion].question);
+            question++;
+
+
+            for (let i = 0; i < historyQuestions[currentQuestion].options.length; i++) {
+                var answerButton = $('<button>');
+                answerButton.html(historyQuestions[currentQuestion].options[i]);
+                answerButton.addClass("answer-button");
+                answerButton.attr('value', historyQuestions[currentQuestion].answer[i]);
+                answerButton.attr('id', i);
+                $('#options').append(answerButton);
+            }
+              
+        $(".answer-button").on("click", function() {
+            console.log($(this).attr("value"));
+            
+        });
+
+    }
+
+
+    }; //END OF startGame();
+
+    startGame();
+
+
+}); //END OF (document).ready
+
